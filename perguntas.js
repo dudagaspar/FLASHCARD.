f43adcc05675c5acfec2e14dc81236ef6eb8f9eb
dudagaspar <00001088697720sp@al.educacao.sp.gov.br>
2024-11-25 20:27:01 -0300

    SALVO

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'CONTO DA BRANCA DE NEVE E OS SETE ANÕES',
+    'Qual é a história principal do filme?',
+    'O filme conta a história de Branca de Neve, uma jovem princesa cuja madrasta, a Rainha Má, sente inveja de sua beleza e tenta matá-la. Branca de Neve foge para a floresta, onde encontra refúgio na casa dos sete anões, mas a Rainha usa uma maçã envenenada para colocá-la em um sono profundo, que só é quebrado pelo beijo de amor verdadeiro do príncipe.'
+)
+
+criaCartao(
+    'CONTO DA BRANCA DE NEVE E OS SETE ANÕES',
+    'Quem são os sete anões?',
+    'Os sete anões são: Mestre, Dengoso, Feliz, Soneca, Atchim, Zangado e Dunga. Cada um tem uma personalidade distinta e traz um toque de humor à história.'
+)
+
+criaCartao(
+    'CONTO DA BRANCA DE NEVE E OS SETE ANÕES',
+    'Por que a Rainha Má odeia Branca de Neve?',
+    'A Rainha Má é obcecada em ser "a mais bela de todas" e, ao descobrir pelo Espelho Mágico que Branca de Neve é mais bela que ela, decide eliminá-la.'
+)
+
+criaCartao(
+    'CONTO DA BRANCA DE NEVE E OS SETE ANÕES',
+    'O que acontece com a Rainha Má no final?',
+    'A Rainha Má é perseguida pelos anões e acaba caindo de um penhasco durante uma tempestade, sendo derrotada.'
+)
